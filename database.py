eaac9af00db35a63bd49808e9017621140e0c9d1
Marius <1jamgochian@gmail.com>
2026-04-23 00:17:56 +0200

    test hardcoded url

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
-DATABASE_URL = os.environ.get('DATABASE_URL') or os.environ.get('DATABASE_PUBLIC_URL')
+DATABASE_URL = "postgresql://postgres:jwsMqrNEFqNmBpeiddHvZGixGwCujlrB@postgres.railway.internal:5432/railway"
 if not DATABASE_URL:
     import sys
 print(f"DATABASE_URL value: '{DATABASE_URL}'", file=sys.stderr)
